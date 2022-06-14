73557706cbb4a090581a5ab137a88bc4343503c3
1-Infiniteloop <skjyeung@gmail.com>
2022-06-14 17:21:57 +0100

    refactor ArticleCard to render body of article as required for EachArticle page

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,13 +1,16 @@
 const ArticleCard = ({ article }) => {
   return (
-    <li className="ArticleCard">
-      <p className="articleCardTopic">{article.topic}</p>
-      <h2 className="articleCardTitle">{article.title}</h2>
-      <p className="articleCardAuthor">Author: {article.author}</p>
-      <p className="articleCardCreatedAt">Created at: {article.created_at}</p>
-      <p className="articleCardVotes">{article.votes} votes</p>
-      <p className="articleCardComments">{article.comment_count} comments</p>
-    </li>
+    <>
+      <li className="ArticleCard">
+        <p className="articleCardTopic">{article.topic}</p>
+        <h2 className="articleCardTitle">{article.title}</h2>
+        <p className="articleCardAuthor">Author: {article.author}</p>
+        <p className="articleCardCreatedAt">Created at: {article.created_at}</p>
+        <p className="articleCardBody">{article.body}</p>
+        <p className="articlesCardVotes">{article.votes} votes</p>
+        <p className="articlesCardComments">{article.comment_count} comments</p>
+      </li>
+    </>
   );
 };
 export default ArticleCard;
