932414f20bf200bee1131313a746d9aae5384cfd
1-Infiniteloop <skjyeung@gmail.com>
2022-06-14 12:57:54 +0100

    remove dayjs formatting

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -6,9 +6,7 @@ const ArticleCard = ({ article }) => {
       <p className="articleCardTopic">{article.topic}</p>
       <h2 className="articleCardTitle">{article.title}</h2>
       <p className="articleCardAuthor">Author: {article.author}</p>
-      <p className="articleCardCreatedAt">
-        Created at: dayjs().format({article.created_at})
-      </p>
+      <p className="articleCardCreatedAt">Created at: {article.created_at}</p>
       <p className="articleCardVotes">{article.votes} votes</p>
       <p className="articleCardComments">{article.comment_count} comments</p>
     </li>
