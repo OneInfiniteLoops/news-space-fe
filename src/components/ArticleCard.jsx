b7b6484edd4426b7bab5ad4d6f598836ff559d13
1-Infiniteloop <skjyeung@gmail.com>
2022-06-15 14:41:51 +0100

    remove articleCardVotes p tag and replace with VoteCounter

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -10,7 +10,6 @@ const ArticleCard = ({ article }) => {
         <p className="articleCardCreatedAt">Created at: {article.created_at}</p>
         <p className="articleCardBody">{article.body}</p>
         <VoteCounter votes={article.votes} article_id={article.article_id} />
-        <p className="articlesCardVotes">{article.votes} votes</p>
         <p className="articlesCardComments">{article.comment_count} comments</p>
       </li>
     </>
