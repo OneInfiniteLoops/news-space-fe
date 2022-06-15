7828a1e45201217d8676dba36d2d1a945427ae90
1-Infiniteloop <skjyeung@gmail.com>
2022-06-15 14:17:04 +0100

    add VoteCounter component as a child of ArticleCard

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,3 +1,5 @@
+import VoteCounter from "./VoteCounter";
+
 const ArticleCard = ({ article }) => {
   return (
     <>
@@ -7,6 +9,7 @@ const ArticleCard = ({ article }) => {
         <p className="articleCardAuthor">Author: {article.author}</p>
         <p className="articleCardCreatedAt">Created at: {article.created_at}</p>
         <p className="articleCardBody">{article.body}</p>
+        <VoteCounter votes={article.votes} article_id={article.article_id} />
         <p className="articlesCardVotes">{article.votes} votes</p>
         <p className="articlesCardComments">{article.comment_count} comments</p>
       </li>
