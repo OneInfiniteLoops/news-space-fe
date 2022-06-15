77660b9b423b5cf7e221350fd43edfbc87c5eec5
1-Infiniteloop <skjyeung@gmail.com>
2022-06-15 11:05:49 +0100

    add state for hasError to return error message if article does not exist

--- src/pages/EachArticle.jsx
+++ src/pages/EachArticle.jsx
@@ -7,14 +7,22 @@ const EachArticle = () => {
   const { article_id } = useParams();
   const [article, setArticle] = useState({});
   const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
 
   useEffect(() => {
     getArticleByID(article_id).then(({ article }) => {
-      setArticle(article);
-      setIsLoading(false);
+      if (article) {
+        setArticle(article);
+        setIsLoading(false);
+      } else if (!article) {
+        setHasError(true);
+        setIsLoading(false);
+      }
     });
   }, [article_id]);
 
+  if (hasError)
+    return <p className="errorMsg"> Uh oh! Article does not exist. </p>;
   if (isLoading) return <p className="loadingMsg">Fetching Article...</p>;
 
   return (
