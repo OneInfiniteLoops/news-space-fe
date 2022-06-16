b51295cc07820256a272ea4b81db360eaa10175e
1-Infiniteloop <skjyeung@gmail.com>
2022-06-16 11:52:03 +0100

    allow to display current username from context

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,14 @@
+import { useContext } from "react";
+import { UserContext } from "../contexts/User";
+
 const Header = () => {
-  return <h1 className="Header">NewsSpace/</h1>;
+  const { user, setUser } = useContext(UserContext);
+  console.log(user);
+
+  return (
+    <div className="Header">
+      <h1>NewsSpace/ {user}</h1>
+    </div>
+  );
 };
 export default Header;
