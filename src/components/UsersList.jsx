0ef87dbaabc27e9e23111263f2cd69d41f00c2f3
1-Infiniteloop <skjyeung@gmail.com>
2022-06-16 11:48:47 +0100

    add UsersList component to render list of usernames and buttons

---
+++ src/components/UsersList.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect, useContext } from "react";
+import { getUsers } from "../utils/api";
+import { UserContext } from "../contexts/User";
+
+const UsersList = () => {
+  const [usersList, setUsersList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const { user, setUser } = useContext(UserContext);
+
+  useEffect(() => {
+    getUsers().then(({ users }) => {
+      setUsersList(users);
+      setIsLoading(false);
+    });
+  }, []);
+
+  const handleClick = (username) => {
+    console.log(username);
+    setUser(username);
+  };
+
+  if (isLoading) return <p className="loadingMsg">Fetching Users...</p>;
+
+  return (
+    <>
+      <h2>Profile</h2>
+      <ul className="UsersList">
+        {usersList.map((user) => {
+          return (
+            <div key={user.username}>
+              <li className="username">
+                <h3>{user.username}</h3>
+                <button onClick={() => handleClick(user.username)}>
+                  Select
+                </button>
+              </li>
+            </div>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
+export default UsersList;
