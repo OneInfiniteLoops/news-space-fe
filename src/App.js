5389fe005de1fff157bbc6e5c24be1ab620a753c
1-Infiniteloop <skjyeung@gmail.com>
2022-06-16 11:50:53 +0100

    add new route to switch user profile

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import "./App.css";
-
-import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 import EachArticle from "./pages/EachArticle";
+import SwitchUserProfile from "./pages/SwitchUserProfile";
+import { useContext, useState } from "react";
+import { UserContext } from "./contexts/User";
 
 function App() {
+  const [user, setUser] = useState("jimbo");
+
   return (
     <BrowserRouter>
-      <div className="App">
-        <Header />
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/:topic" element={<Home />} />
-          <Route path="/articles/:article_id" element={<EachArticle />} />
-        </Routes>
-      </div>
+      <UserContext.Provider value={{ user, setUser }}>
+        <div className="App">
+          <Header />
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/:topic" element={<Home />} />
+            <Route path="/articles/:article_id" element={<EachArticle />} />
+            <Route path="/users" element={<SwitchUserProfile />} />
+          </Routes>
+        </div>
+      </UserContext.Provider>
     </BrowserRouter>
   );
 }
