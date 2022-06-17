b52d6c111d0647bb44da173ef71e88ba5173f594
1-Infiniteloop <skjyeung@gmail.com>
2022-06-17 22:35:33 +0100

    centering elements for mobile ui

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -25,26 +25,30 @@ const UsersList = () => {
   if (isLoading) return <p className="loadingMsg">Fetching Users...</p>;
 
   return (
-    <div className="switch-profile-panel">
-      <h2 className="profile-heading">Profile</h2>
-      <ul className="UsersList">
-        {usersList.map((user) => {
-          return (
-            <div key={user.username}>
-              <li className="username">
-                <h3>{user.username}</h3>
-                <button onClick={() => handleSelectClick(user.username)}>
-                  Select User
-                </button>
-              </li>
-            </div>
-          );
-        })}
-      </ul>
-      <button className="sign-out-button" onClick={handleSignOutClick}>
-        Sign out
-      </button>
-    </div>
+    <>
+      <div className="switch-profile-panel">
+        <h2 className="profile-heading">Profile</h2>
+        <ul className="UsersList">
+          {usersList.map((user) => {
+            return (
+              <div key={user.username}>
+                <li className="username">
+                  <h3>{user.username}</h3>
+                  <button onClick={() => handleSelectClick(user.username)}>
+                    Select User
+                  </button>
+                </li>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="sign-out">
+        <button className="sign-out-button" onClick={handleSignOutClick}>
+          Sign out
+        </button>
+      </div>
+    </>
   );
 };
 export default UsersList;
