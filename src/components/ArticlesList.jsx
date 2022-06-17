d5617ffbf59f1ee811f088469f512e8e8931ef19
1-Infiniteloop <skjyeung@gmail.com>
2022-06-17 11:23:29 +0100

    remove import from ArticleCard

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import { getArticles } from "../utils/api";
-import ArticleCard from "./ArticleCard";
 
 const ArticlesList = () => {
   const { topic } = useParams();
